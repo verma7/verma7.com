6f79b90a43d2ca87ac5e1e678f26adae32fc85c3
Abhishek Verma <vermaa@vermaa-macbookpro.roam.corp.google.com>
2018-04-30 01:04:54 -0700

    Remove the PHP engine

--- engine/personal.php
+++
@@ -1,8 +0,0 @@
-<?php	include_once("header.php");
-	echo getTopBar("personal");
-?>
-	<div class="textContainer">
-
-Coming soon.
-
-<?php	include_once("footer.html"); ?>
--- engine/process.sh
+++
@@ -1,5 +0,0 @@
-php index.php | grep -v "Content-type: text/html" > ../index.html
-php pub.php | grep -v "Content-type: text/html" > ../pub.html
-php personal.php | grep -v "Content-type: text/html" > ../personal.html
-php prof.php | grep -v "Content-type: text/html" > ../prof.html
-php travel.php | grep -v "Content-type: text/html" > ../travel.html
